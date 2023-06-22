80840a3b66bb209264688bbf6ecc055e9908a316
Gerald Versluis <gerald.versluis@microsoft.com>
2023-06-22 10:30:12 +0200

    Initial commit

---
+++ MauiCustomCursorColoriOSSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCustomCursorColoriOSSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiCustomCursorColoriOSSample/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiCustomCursorColoriOSSample;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("CursorColor", (handler, view) =>
+        {
+#if IOS
+			handler.PlatformView.TintColor = UIKit.UIColor.Green;
+#endif
+        });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
